b780a16351cef60263de0d22a29f3c275a1b8b2d
8uNny29 <rafirabani32@gmail.com>
2024-01-26 20:52:59 +0700

    Sedikit penambahan fitur

--- app.js
+++ app.js
@@ -13,6 +13,8 @@ const port = 2903;
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
+let formData;
+
 // Konfigurasi database
 const db = mysql.createConnection({
   host: '127.0.0.1',
@@ -36,7 +38,8 @@ function generateVerificationCode() {
   return Math.floor(100000 + Math.random() * 900000);
 }
 
-app.post('/', (req, res) => {
+app.post('/', (req, res, next) => {
+  formData = req.body;
   const studentName = req.body.studentName;
   const verificationCode = generateVerificationCode();
   const qr_png = qr.image(verificationCode, { type: 'png' });
@@ -46,14 +49,16 @@ app.post('/', (req, res) => {
   console.log(verificationCode);
 });
 
-app.post('/sudahabsen', (req, res) => {
+app.post('/sudahabsen', (req, res, next) => {
   const verificationCode = req.body.kodeVerif;
-  const studentName = req.app.studentName;
-  console.log(studentName + ' Telah memasukkan kode: ' + verificationCode);
-  res.send('Data absensi tersimpan untuk ' + studentName + ' menggunakan kode: ' + verificationCode);
+  db.query('INSERT INTO absensi (nama_murid) VALUES (?)', [formData.studentName], (err, result) => {
+    if (err) throw err;
+    console.log(`${formData.studentName} Telah memasukkan kode: ${verificationCode}`);
+    res.send(`Data absensi tersimpan untuk ${formData.studentName} menggunakan kode: ${verificationCode}`);
+    });
 });
 
-//app.get('/masukankode/:studentName', (req, res) => {
+//app.get('/scan/:studentName', (req, res) => {
 //  const studentName = req.params.studentName;
 //  db.query('INSERT INTO absensi (nama_murid) VALUES (?)', [studentName], (err, result) => {
 //    if (err) throw err;
