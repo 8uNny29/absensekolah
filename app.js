2eafdcdbd0f2062cd3b29b0c0a2d351ae2d45488
8uNny29 <rafirabani32@gmail.com>
2024-01-27 05:47:05 +0700

    Added some feature and some function

--- app.js
+++ app.js
@@ -6,13 +6,16 @@ const bodyParser = require('body-parser');
 const fs = require('fs');
 const mysql = require('mysql'); // Anda perlu menginstal paket mysql dengan npm install mysql
 const { get } = require('http');
+const path = require('path');
 
 const app = express();
-const port = 2903;
+const port = 3000;
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
+app.use(express.static(path.join(__dirname, '')));
 
+// Function for get some value from other process
 let formData;
 
 // Konfigurasi database
@@ -34,27 +37,31 @@ app.get('/', (req, res) => {
   res.sendFile(__dirname + '/index.html');
 });
 
-function generateVerificationCode() {
+app.get('/style.css', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'style.css'));
+});
+
+function generatekodeVerif() {
   return Math.floor(100000 + Math.random() * 900000);
 }
 
 app.post('/', (req, res, next) => {
   formData = req.body;
   const studentName = req.body.studentName;
-  const verificationCode = generateVerificationCode();
-  const qr_png = qr.image(verificationCode, { type: 'png' });
+  const kodeVerif = generatekodeVerif();
+  const qr_png = qr.image(kodeVerif, { type: 'png' });
   const qrPath = 'qr_codes/' + studentName + '.png';
   qr_png.pipe(fs.createWriteStream(qrPath));
-  console.log(studentName + ' Telah Membuat QR Code');
-  console.log(verificationCode);
+  console.log(studentName + ' Telah membuat QR Code');
+  console.log(kodeVerif);
 });
 
 app.post('/sudahabsen', (req, res, next) => {
-  const verificationCode = req.body.kodeVerif;
+  const kodeVerif = req.body.kodeVerif;
   db.query('INSERT INTO absensi (nama_murid) VALUES (?)', [formData.studentName], (err, result) => {
     if (err) throw err;
-    console.log(`${formData.studentName} Telah memasukkan kode: ${verificationCode}`);
-    res.send(`Data absensi tersimpan untuk ${formData.studentName} menggunakan kode: ${verificationCode}`);
+    console.log(`${formData.studentName} telah memasukkan kode: ${kodeVerif}`);
+    res.send(`Data absensi mu telah tersimpan, menggunakan kode: ${kodeVerif}`);
   });
 });
 
